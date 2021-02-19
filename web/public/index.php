2f4dd6e6e209f4f21d9475e699f5413eb226434e
yasuaki640 <yasuaki640@gmail.com>
2021-02-19 22:23:06 +0900

    設定をいじる

--- web/public/index.php
+++ web/public/index.php
@@ -5,11 +5,12 @@ $foo = new App\Acme\Foo();
 
 ?><!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Docker <?php echo $foo->getName(); ?></title>
-    </head>
-    <body>
-        <h1>Docker <?php echo $foo->getName(); ?></h1>
-    </body>
+<!--    <head>-->
+<!--        <meta charset="utf-8">-->
+<!--        <title>Docker --><?php //echo $foo->getName(); ?><!--</title>-->
+<!--    </head>-->
+<!--    <body>-->
+<!--        <h1>Docker --><?php //echo $foo->getName(); ?><!--</h1>-->
+<!--    </body>-->
+<?php phpinfo() ?>
 </html>
