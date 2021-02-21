6fba5da5b8bbe27d4293dc51fc8a00b8709f78dd
yasuaki640 <yasuaki640@gmail.com>
2021-02-21 19:30:59 +0900

    練習問題4.1

---
+++ web/public/chapter4/Ex4_1.php
@@ -0,0 +1,20 @@
+<?php
+
+$execute_class = new Ex4_1();
+print $execute_class->getEvaluation(78);
+
+class Ex4_1
+{
+    function getEvaluation(int $point): string
+    {
+        if ($point >= 90) {
+            return '優';
+        } else if ($point >= 70) {
+            return '良';
+        } else if ($point >= 50) {
+            return '可';
+        } else {
+            return '不可';
+        }
+    }
+}
