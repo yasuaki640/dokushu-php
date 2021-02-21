e40e527794d2532911b080748a91f560f05cdfe3
yasuaki640 <yasuaki640@gmail.com>
2021-02-21 11:19:02 +0900

    ;lzsdkj

---
+++ web/public/chapter2/bracket.php
@@ -0,0 +1,6 @@
+<?php
+$names[] = "kissse michiko";
+$names[] = "abe kayako";
+$names[] = "miyoshi ayaka";
+$names[] = "honda tsubasa";
+print_r($names);
---
+++ web/public/chapter2/dimension.php
@@ -0,0 +1,20 @@
+<?php
+$data = [
+    [
+        'name' => 'kayako abe',
+        'age' => 24,
+        'sex' => 'male'
+    ],
+    [
+        'name' => 'ayaka miyoshi',
+        'age' => 26,
+        'sex' => 'male'
+    ],
+    [
+        'name' => 'tsubasa honda',
+        'age' => 26,
+        'sex' => 'male'
+    ],
+];
+
+print_r($data);
