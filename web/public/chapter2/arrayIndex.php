ca502939c59ab6f670a748a7bcb94d5c2b20b462
yasuaki640 <yasuaki640@gmail.com>
2021-02-21 11:25:52 +0900

    phpの配列は気持ち悪い、、

---
+++ web/public/chapter2/arrayIndex.php
@@ -0,0 +1,9 @@
+<?php
+$data = [1 => 'tako', 4 => 'ika', 'kani'];
+print_r($data);
+
+$himonos = [-10 => 'surume', 'iwashi', 'aji'];
+print_r($himonos);
+
+$kimoi_hairetsu = ['tako' => 'umi', 29 => 'riku', 'buta' => 'riku', 'sora'];
+print_r($kimoi_hairetsu);
