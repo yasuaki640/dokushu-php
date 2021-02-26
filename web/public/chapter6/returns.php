d0f82ea704bf68a0f58327a58b8fdc68399fe4fd
yasuaki640 <yasuaki640@gmail.com>
2021-02-26 22:04:04 +0900

    ；ｌｋｊ

--- web/public/chapter6/returns.php
+++ web/public/chapter6/returns.php
@@ -1,1 +1,13 @@
 <?php
+declare(strict_types=1);
+
+function max_min(float ...$args): array
+{
+    return [max($args), min($args)];
+}
+
+$result = max_min(10, 2, -45, 456);
+print_r($result);
+
+list($max,$min) = max_min(10, 2, -45, 456);
+print "max : {$max} min : {$min}";
