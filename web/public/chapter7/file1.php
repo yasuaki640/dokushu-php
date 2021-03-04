a5ed0f70bbf0c2807ed4d8e5811f1773332fdfdc
yasuaki640 <yasuaki640@gmail.com>
2021-03-04 21:27:33 +0900

    ファイルアップロード

---
+++ web/public/chapter7/file1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>File upload</title>
+</head>
+<body>
+<form method="POST" action="file2.php" enctype="multipart/form-data">
+    <label for="upfile">add file path : </label>
+    <input type="hidden" name="max_file_size" value="1000000">
+    <input id="upfile" type="file" name="upfile" size="40">
+    <input type="submit" value="upload">
+</form>
+</body>
+</html>
+
+
---
+++ web/public/chapter7/file2.php
@@ -0,0 +1,34 @@
+<?php
+$ext = pathinfo($_FILES['upfile']['name']);
+
+$perm = ['gif', 'jpg', 'jpeg', 'png'];
+
+if ($_FILES['upfile']['error'] !== UPLOAD_ERR_OK) {
+    $msg = [
+        UPLOAD_ERR_INI_SIZE => 'php.iniのファイルサイズ制限を超えてる',
+        UPLOAD_ERR_FORM_SIZE => 'HTMLのMAX_FILE_SIZEを超えています',
+        UPLOAD_ERR_PARTIAL => '一部しかアップロードされませんでした',
+        UPLOAD_ERR_NO_TMP_DIR => '一次保存フォルダが存在しません',
+        UPLOAD_ERR_CANT_WRITE => 'ディスクへの書き込みに失敗しました',
+        UPLOAD_ERR_EXTENSION => '拡張モジュールによってアップロードが中断されました'
+    ];
+    $err_msg = $msg[$_FILES['upfile']['error']];
+
+} elseif (!in_array(strtolower($ext['extension']), $perm)) {
+    $err_msg = '画像以外はアップロードできません';
+} elseif (!@getimagesize($_FILES['upfile']['tmp_name'])) {
+    $err_msg = 'ファイルの内容が画像ではありません';
+} else {
+    $src = $_FILES['upfile']['tmp_name'];
+    $dest = mb_convert_encoding($_FILES['upfile']['name'], 'UTF-8');
+
+    if (!move_uploaded_file($src, 'doc/' . $dest)) {
+        $err_msg = 'アップロード処理に失敗しました';
+    }
+}
+
+if (isset($err_msg)) {
+    die($err_msg);
+}
+
+header('Location: http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/file1.php');
