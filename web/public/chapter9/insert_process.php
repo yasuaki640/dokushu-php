1f6ff4eee770c312c543006177e81769d2d8bcb2
yasuaki640 <yasuaki640@gmail.com>
2021-03-08 11:04:26 +0900

    DBへの登録フォーム

---
+++ web/public/chapter9/insert_process.php
@@ -0,0 +1,18 @@
+<?php
+require_once './DbManager.php';
+
+try {
+    $db = getDb();
+    $stt = $db->prepare('INSERT INTO book(isbn, title, price, publish, published) VALUES(:isbn,:title,:price,:publish,:published)');
+    $stt->bindValue(':isbn', $_POST['isbn']);
+    $stt->bindValue(':title', $_POST['title']);
+    $stt->bindValue(':price', $_POST['price']);
+    $stt->bindValue(':publish', $_POST['publish']);
+    $stt->bindValue(':published', $_POST['published']);
+    $stt->execute();
+
+    header('Location: http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/insert_form.html');
+
+} catch (PDOException $e) {
+    print 'error has occurred : ' . $e->getMessage();
+}
