a8e3932c42db204efab7914299335ff5f080b44f
yasuaki640 <yasuaki640@gmail.com>
2021-03-08 11:30:57 +0900

    fetch all

--- web/public/chapter9/result.php
+++ web/public/chapter9/result.php
@@ -1,1 +1,42 @@
 <?php
+require_once '../DbManager.php';
+require_once '../Encode.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>result set</title>
+</head>
+<body>
+<table border="1">
+    <tr>
+        <th>ISBN code</th>
+        <th>Title</th>
+        <th>Price</th>
+        <th>Publish</th>
+        <th>Published</th>
+    </tr>
+    <?php
+    try {
+        $db = getDb();
+        $stt = $db->prepare('SELECT * FROM book ORDER BY published DESC');
+        $stt->execute();
+        while ($row = $stt->fetch(PDO::FETCH_ASSOC)) {
+            ?>
+            <tr>
+                <td><?= e($row['isbn']) ?></td>
+                <td><?= e($row['title']) ?></td>
+                <td><?= e($row['price']) ?></td>
+                <td><?= e($row['publish']) ?></td>
+                <td><?= e($row['published']) ?></td>
+            </tr>
+            <?php
+        }
+    } catch (PDOException $e) {
+        print $e->getMessage();
+    }
+    ?>
+</table>
+</body>
+</html>
