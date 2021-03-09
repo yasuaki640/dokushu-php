b8f9b836c7788a36975e24b595e6e7753127a7f4
yasuaki640 <yasuaki640@gmail.com>
2021-03-09 09:19:35 +0900

    あ、こっちが匿名クラス

---
+++ web/public/chapter10/anonymous_class.php
@@ -0,0 +1,25 @@
+<?php
+
+interface Runnable
+{
+    function run();
+}
+
+class MyClass
+{
+    public function execute(Runnable $rc)
+    {
+        print 'start...';
+        $rc->run();
+        print 'end...';
+    }
+}
+
+$cls = new MyClass();
+
+$cls->execute(new class implements Runnable {
+    function run()
+    {
+        print 'process..';
+    }
+});
